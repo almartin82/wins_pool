4f1aa3cacb2bf364f333402db106e8738c52036e
almartin82 <almartin@gmail.com>
2014-09-06 12:24:33 -0400

    simple team response

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask.ext import restful
+import nfl_teams
 
 app = Flask(__name__)
 api = restful.Api(app)
 
+#objects
 class HelloWorld(restful.Resource):
     def get(self):
         return {'hello': 'world'}
 
+class nfl_teams(restful.Resource):
+    def get(self):
+        return jsonify(nfl_teams)
+
+#endpoints
 api.add_resource(HelloWorld, '/')
+api.add_resource(nfl_teams, '/nfl')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
