f0e473d90efc3b1a2fa8befcfa3a6738c652de11
almartin82 <almartin@gmail.com>
2014-09-06 10:59:36 -0400

    following diff minimal ex

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
+import os
 from flask import Flask
-from flask.ext import restful
-
 app = Flask(__name__)
-api = restful.Api(app)
 
-class HelloWorld(restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
