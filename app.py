3dc7d4f5d456eb64d3c7a0fc1e3700875067a792
almartin82 <almartin@gmail.com>
2014-09-06 11:09:10 -0400

    revert to old minimal app

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-import os
 from flask import Flask
+from flask.ext import restful
+
 app = Flask(__name__)
+api = restful.Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
