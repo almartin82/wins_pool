8d5c9eabb2f40f9fd1dc8f0c08c76b7fd818bd3d
almartin82 <almartin@gmail.com>
2014-09-06 12:33:08 -0400

    jsonify the resp

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask.ext import restful
 import nfl_teams
 
@@ -9,11 +9,11 @@ api = restful.Api(app)
 #objects
 class HelloWorld(restful.Resource):
     def get(self):
-        return nfl_teams
+        return {'hello': 'world'}
 
 class nfl(restful.Resource):
     def get(self):
-        return nfl_teams
+        return jsonify(nfl_teams)
 
 #endpoints
 api.add_resource(HelloWorld, '/')
